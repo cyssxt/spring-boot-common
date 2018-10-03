bb68f548c7974ac593baf2a65405b3738b1d2499
cyssxt <cyssxt@163.com>
2018-10-03 19:59:12 +0800

    基线代码设置

--- src/main/java/com/cyssxt/common/message/MessageHelper.java
+++ src/main/java/com/cyssxt/common/message/MessageHelper.java
@@ -3,53 +3,100 @@ package com.cyssxt.common.message;
 import com.cyssxt.common.message.bean.MessageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Properties;
+
+import java.io.*;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MessageHelper {
 
     private final static Logger logger = LoggerFactory.getLogger(MessageHelper.class);
     public static final String DEFAULT = "default";
+    private final static Pattern LANG_REG = Pattern.compile("message/([a-zA-Z]+).properties");
 
-    private String lang;
+    private MessageHelper(){}
 
-    private MessageHelper(String lang) {
-        this.lang = lang;
+    private String defaultLang;
+
+    private MessageHelper(String defaultLang) {
+        this.defaultLang = defaultLang;
     }
 
     private final static Map<String,Map<String,String>> langMap = new HashMap<>();
-    private final static Map<String,MessageHelper> helpers = new HashMap<>();
+    private final static Map<String,Message> messages = new HashMap<>();
 
     static {
         try {
             init();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static MessageHelper getInstance(){
+    private static class Message{
+        private String lang;
+
+        public Message(String lang) {
+            this.lang = lang;
+        }
+
+        private String getLang() {
+            return lang==null?DEFAULT:lang;
+        }
+
+        public MessageInfo getMessageInfo(String key,String lang){
+            String value = getString(lang);
+            return new MessageInfo(key,value);
+        }
+
+        public String getString(String key,String lang){
+            Map<String,String> map = langMap.get(lang);
+            if(null==map){
+                map = langMap.get(DEFAULT);
+            }
+            String result = null;
+            if(null!=map){
+                result = map.get(key);
+            }
+            return result;
+        }
+
+        public String getString(String key){
+            return getString(key,getLang());
+        }
+
+        public Integer getInteger(String key,String lang){
+            String value = getString(key,lang);
+            return Integer.valueOf(value);
+        }
+
+        public Integer getInteger(String key){
+            return getInteger(key,getLang());
+        }
+    }
+
+    public static Message getInstance(){
         return getInstance(DEFAULT);
     }
 
-    public static MessageHelper getInstance(String lang){
+    public static Message getInstance(String lang){
         if(!StringUtils.isEmpty(lang)){
             lang = DEFAULT;
         }
-        MessageHelper helper = helpers.get(lang);
-        if(null==helper){
-            helper = new MessageHelper(lang);
-            helpers.put(lang,helper);
+        Message message = messages.get(lang);
+        if(null==message){
+            message = new Message(lang);
+            messages.put(lang,message);
         }
-        return helper;
+        return message;
     }
 
     public static void initLang(File file) throws IOException {
@@ -59,10 +106,18 @@ public class MessageHelper {
         if(!StringUtils.isEmpty(fileName)) {
             lang = fileName;
         }
+        initWithIS(new FileInputStream(file), lang);
+    }
+
+    private static void initWithIS(InputStream is,String lang) throws IOException {
         Properties properties = new Properties();
-        properties.load(new FileInputStream(file));
+        properties.load(is);
         Iterator iterator = properties.keySet().iterator();
-        Map<String,String> map = new HashMap<>();
+        Map<String,String> map = langMap.get(lang);
+        if(map==null){
+            map = new HashMap<>();
+            langMap.put(lang,map);
+        }
         while(iterator.hasNext()){
             String key = (String)iterator.next();
             String value = properties.getProperty(key);
@@ -71,52 +126,48 @@ public class MessageHelper {
         langMap.put(lang,map);
     }
 
-    public static void init() throws FileNotFoundException {
-        File pFile = ResourceUtils.getFile("classpath:message");
-        if(pFile.isDirectory()){
-            File[] files  = pFile.listFiles();
-            for(File file:files){
-                try {
-                    initLang(file);
-                } catch (IOException e) {
-                    logger.error("init ={}",e);
+    public static void init() throws IOException {
+        ClassLoader classLoader = MessageHelper.class.getClassLoader();
+        Enumeration<URL> is = classLoader.getResources("message");
+        while (is!=null && is.hasMoreElements()){
+            URL url = is.nextElement();
+            String protocol = url.getProtocol();
+            if("file".equals(protocol)){
+                File pFile = new File(url.getPath());
+                if(pFile.isDirectory()){
+                    File[] files  = pFile.listFiles();
+                    for(File file:files){
+                        try {
+                            initLang(file);
+                        } catch (IOException e) {
+                            logger.error("init ={}",e);
+                        }
+                    }
+                }
+            }else if("jar".equals(protocol)){
+                //遍历获取jar中的国际化内容
+                JarFile jar = ((JarURLConnection) url.openConnection()).getJarFile();
+                Enumeration<JarEntry> entries = jar.entries();
+                while (entries.hasMoreElements()) {
+                    JarEntry entry = entries.nextElement();
+                    if (entry.isDirectory()) {
+                        continue;
+                    }
+                    String name = entry.getName();
+                    Matcher matcher = LANG_REG.matcher(name);
+                    if(matcher.find()){
+                        String lang = matcher.group(1);
+                        InputStream inputStream = classLoader.getResourceAsStream(name);
+                        initWithIS(inputStream,lang);
+                    }
                 }
             }
         }
     }
 
-    private String getLang() {
-        return lang==null?DEFAULT:lang;
-    }
-
-    public Integer getInteger(String key,String lang){
-        String value = getString(key,lang);
-        return Integer.valueOf(value);
-    }
-
-    public Integer getInteger(String key){
-        return getInteger(key,getLang());
-    }
-
-    public String getString(String key,String lang){
-        Map<String,String> map = langMap.get(lang);
-        if(null==map){
-            map = langMap.get(DEFAULT);
-        }
-        String result = null;
-        if(null!=map){
-            result = map.get(key);
-        }
-        return result;
-    }
-
-    public MessageInfo getMessageInfo(String key,String lang){
-        String value = getString(lang);
-        return new MessageInfo(key,value);
-    }
-
-    public String getString(String key){
-        return getString(key,getLang());
+    public static String getString(String key,String lang){
+        Message message = getInstance(lang);
+        return message.getString(key);
     }
 
     public static void main(String[] args) {
