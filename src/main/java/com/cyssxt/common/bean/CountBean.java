ed3ef94bc3ad34b1871ae02a2b47a13a597f6659
cyssxt <cyssxt@163.com>
2018-11-10 15:18:16 +0800

    增加统计

--- src/main/java/com/cyssxt/common/bean/CountBean.java
+++ src/main/java/com/cyssxt/common/bean/CountBean.java
@@ -5,8 +5,22 @@ import lombok.Data;
 @Data
 public class CountBean {
     private Long count;
+    private Long count2;
+
+    private String rowId;
 
     public CountBean(Long count) {
         this.count = count;
+    }
+
+    public CountBean(Long count, String rowId) {
+        this.count = count;
+        this.rowId = rowId;
+    }
+
+    public CountBean(Long count, Long count2, String rowId) {
+        this.count = count;
+        this.count2 = count2;
+        this.rowId = rowId;
     }
 }
