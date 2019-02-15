144275e94d3eb112c9bccd4381d7b2aac4aa18eb
cyssxt <cyssxt@163.com>
2019-02-15 11:41:43 +0800

    拷贝增加过滤方法

--- src/main/java/com/cyssxt/common/bean/Copy.java
+++ src/main/java/com/cyssxt/common/bean/Copy.java
@@ -4,7 +4,6 @@ import com.cyssxt.common.exception.ValidException;
 import com.cyssxt.common.utils.ReflectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.Map;
@@ -12,6 +11,11 @@ import java.util.Map;
 public class Copy {
     private final Logger logger = LoggerFactory.getLogger(Copy.class);
     public <T>T parse(T t) throws ValidException {
+        parse(t, (key, o) -> o!=null || "expireTime".equals(key));
+        return t;
+    }
+
+    public <T>T parse(T t,Filter filter) throws ValidException {
         Map<String,Method> readMethods = null;
         Map<String,Method> writeMethods = null;
         try {
@@ -23,7 +27,7 @@ public class Copy {
                 Method read = readMethods.get(key);
                 Object object = read.invoke(this);
                 Method writeMethod =writeMethods.get(key);
-                if((object!=null || "expireTime".equals(key)) && writeMethod!=null){
+                if((filter.valid(key,object) && writeMethod!=null)){
                     logger.debug("parse,fieldName={}",key);
                     writeMethod.invoke(t,object);
                 }
@@ -35,4 +39,7 @@ public class Copy {
         return t;
     }
 
+    public interface Filter {
+        boolean valid(String key,Object o);
+    }
 }
--- src/main/java/com/cyssxt/common/controller/BaseController.java
+++ src/main/java/com/cyssxt/common/controller/BaseController.java
@@ -1,4 +1,27 @@
 package com.cyssxt.common.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 public class BaseController {
+        private final static Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+        protected HttpServletRequest request;
+        protected HttpServletResponse response;
+        protected HttpSession session;
+
+        @ModelAttribute
+        public void setReqAndRes( HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+            this.request = httpServletRequest;
+            this.response = httpServletResponse;
+            this.session = httpServletRequest.getSession();
+//        this.responseData = ResponseData.getDefaultSuccessResponse(baseReq);
+
+        }
+
 }
